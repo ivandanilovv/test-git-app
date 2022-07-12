09e06c06b27b44b56782fcd7a8bde762e15966ed
ivanadimitrova123 <85631166+ivanadimitrova123@users.noreply.github.com>
2022-07-12 13:25:54 +0200

    installed bootstrap (#4)
    Co-authored-by: ivandanilovv <82410043+ivandanilovv@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '../src/assets/app.scss'
-
+import './assets/scss/app.scss'
 Vue.config.productionTip = false
 
 new Vue({
